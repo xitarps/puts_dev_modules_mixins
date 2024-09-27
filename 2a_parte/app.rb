32716f77461d6dd789b4a3b1b15c3564cf448183
XitaRps <xitarps@gmail.com>
2024-09-27 17:27:34 -0300

    Adds search and as_json features

--- 2a_parte/app.rb
+++ 2a_parte/app.rb
@@ -3,6 +3,9 @@ require_relative 'car'
 class App
   def self.call
     car = Car.new(color: 'red', model: 'ecosport', year: 2005, brand: 'ford')
+    car = Car.new(color: 'blue', model: 'ka', year: 2005, brand: 'ford')
+    car = Car.new(color: 'red', model: 'viper', year: 2000, brand: 'dodge')
+
     binding.irb
   end
 end
--- 2a_parte/car.rb
+++ 2a_parte/car.rb
@@ -1,10 +1,37 @@
 class Car
-  attr_accessor :color, :model, :year, :brand
+  @@dataset = []
 
-  def initialize(color: nil, model: nil, year: nil, brand: nil)
+  attr_accessor :id, :color, :model, :year, :brand
+
+  def initialize(id: nil, color: nil, model: nil, year: nil, brand: nil)
     @color = color
     @model = model
     @year = year
     @brand = brand
+
+    return self unless id.nil?
+
+    @id = @@dataset.size + 1
+    @@dataset << self
+  end
+
+  def self.search(term)
+    result = @@dataset.map(&:as_json).select do |item|
+      item.values.any? { |attr| attr.to_s.include?(term.to_s) }
+    end
+
+    result.map{ |item| self.new(**item) }
+  end
+
+  def as_json
+    # hash = {}
+
+    # instance_variables.each do |variable|
+    #   hash[variable.to_s.gsub('@','')] = send(variable.to_s.gsub('@','').to_sym)
+    # end
+
+    # hash
+
+    {}.tap{ |hash| instance_variables.each{ |var| hash[var.to_s.delete('@').to_sym] = send(var.to_s.gsub('@','').to_sym) } }
   end
 end
